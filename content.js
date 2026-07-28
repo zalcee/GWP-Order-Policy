39f646e2197e2ed72f8b7c5211c6d0e4a2d9b0e9
zalcee <zalceesongodanan18@gmail.com>
2026-07-28 20:09:49 +0800

    For ref code

---
+++ content.js
@@ -0,0 +1,8 @@
+window.addEventListener("load", () => {
+    const btn = document.getElementById("endpoint");
+
+    if (btn) {
+        btn.disabled = true;
+        e.preventDefault();
+    }
+});
