3c0366e860c502c6e388aaaf48a797e3f023c5dc
zalcee <zalcee.songodanan@trcon.ph>
2026-07-29 18:21:43 +0800

    final code

--- content.js
+++ content.js
@@ -1,8 +1,23 @@
-window.addEventListener("load", () => {
-    const btn = document.getElementById("endpoint");
+const disableTenderButton = () => {
+    const btn = document.getElementById("tenderbutton");
 
     if (btn) {
         btn.disabled = true;
-        e.preventDefault();
+        btn.style.pointerEvents = "none";
+        btn.style.opacity = "0.5";
+        btn.title = "Disabled by Chrome Extension";
     }
+}
+
+
+disableTenderButton();
+
+
+const observer = new MutationObserver(() => {
+    disableTenderButton();
+});
+
+observer.observe(document.body, {
+    childList: true,
+    subtree: true
 });
